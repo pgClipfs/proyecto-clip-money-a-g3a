c7216ecb2e0dc665b87e46393684abb7660b8669
M. Ferrero <69654913+FerreroM95@users.noreply.github.com>
2020-10-28 16:05:58 -0300

    modelos moneda y operaciones

---
+++ Clip-Back/Clip-Back/DAL/Entities/Moneda.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clip_Back.DAL.Entities
+{
+    public class Moneda
+    {
+        /// <summary>
+        /// Constructor vacio
+        /// </summary>
+
+        public Moneda()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor de mapeo con entidad modelo
+        /// </summary>
+        /// <param name="moneda"></param>
+        public Moneda(Models.Moneda moneda)
+        {
+            tipo = moneda.tipo;
+            valoractual = moneda.valoractual;
+ 
+        }
+
+
+        /// <summary>
+        /// Representa la columna Id en la tabla moneda
+        /// </summary>
+        [Key]
+        public int id_moneda { get; set; }
+
+        /// <summary>
+        /// Representa la columna tipo en la tabla moneda
+        /// </summary>
+        public string tipo { get; set; }
+
+        /// <summary>
+        /// Representa la columna valoractual en la tabla moneda
+        /// </summary>
+        public int valoractual { get; set; }
+
+    }
+}
