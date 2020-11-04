93baee6a46fc4ffebb6127f8bdc6184350aed67c
Federico <federico_joelvergelli@hotmail.com>
2020-11-04 13:41:29 -0300

    Creacin de la NavBar y el Footer

---
+++ clip-money-front/src/app/components/shared/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.sass']
+})
+export class FooterComponent implements OnInit {
+
+  anio: number;
+
+  constructor() {
+    this.anio = new Date().getFullYear();
+
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
