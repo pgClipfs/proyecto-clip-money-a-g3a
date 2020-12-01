5926ababa86750b88c2e268d7faf3e33abd94f0f
Marcos Cuchian <marcoscuchian@gmail.com>
2020-12-01 20:42:38 -0300

    cambios en routeconfig

--- Clip-Back/Clip-Back/App_Start/BundleConfig.cs
+++ Clip-Back/Clip-Back/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace Clip_Back
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                    "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css"));
         }
     }
 }
--- Clip-Back/Clip-Back/App_Start/RouteConfig.cs
+++ Clip-Back/Clip-Back/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace Clip_Back
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{*.}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
