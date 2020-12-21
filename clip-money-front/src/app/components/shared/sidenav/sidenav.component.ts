6deb3c621481b8dd5a2e3c38fe1228cfbdeb336b
Federico <federico_joelvergelli@hotmail.com>
2020-12-20 22:01:19 -0300

    Implementacion del componente Sidenav

---
+++ clip-money-front/src/app/components/shared/sidenav/sidenav.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @title Autosize sidenav
+ */
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.sass']
+})
+export class SidenavComponent {
+  showFiller = false;
+}
