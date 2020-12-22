6f6362f58457f2b5edd696e6896eb36db91a10e8
Marcos Cuchian <marcoscuchian@gmail.com>
2020-12-22 18:44:38 -0300

    cambios archivos bundles y archivo register2(algunas modificaciones)

--- clip-money-front/src/app/components/shared/register2/register2.component.ts
+++ clip-money-front/src/app/components/shared/register2/register2.component.ts
@@ -8,18 +8,19 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './register2.component.html',
   styleUrls: ['./register2.component.sass']
 })
+
 export class Register2Component implements OnInit {
   returnUrl: string;
   error = '';
-  registerName     = new FormControl('', Validators.required);
-  registerLastName     = new FormControl('', Validators.required);
-  registerEmail     = new FormControl('', Validators.required);
-  registerDni     = new FormControl('', Validators.required);
-  registerHome     = new FormControl('', Validators.required);
-  registerTelephone     = new FormControl('', Validators.required);
-  registerUser     = new FormControl('', Validators.required);
-  registerPassword     = new FormControl('', Validators.required);
-  registerbirthdate     = new FormControl('', Validators.required);
+  registerName = new FormControl('', Validators.required);
+  registerLastName = new FormControl('', Validators.required);
+  registerEmail = new FormControl('', Validators.required);
+  registerDni = new FormControl('', Validators.required);
+  registerHome = new FormControl('', Validators.required);
+  registerTelephone = new FormControl('', Validators.required);
+  registerUser = new FormControl('', Validators.required);
+  registerPassword = new FormControl('', Validators.required);
+  registerbirthdate = new FormControl('', Validators.required);
 
 
   hide = true;
@@ -28,17 +29,19 @@ export class Register2Component implements OnInit {
 
   ngOnInit(): void {
   }
+
+
   onSubmit(): void {
     console.log("llamamos a la funcion onsubmit");
-    console.log(this.registerName,
-      this.registerLastName,
-      this.registerEmail,
-      this.registerDni,
-      this.registerHome,
-      this.registerTelephone,
-      this.registerUser,
-      this.registerPassword,
-      this.registerbirthdate);
+    console.log(this.registerName.value,
+      this.registerLastName.value,
+      this.registerEmail.value,
+      this.registerDni.value,
+      this.registerHome.value,
+      this.registerTelephone.value,
+      this.registerUser.value,
+      this.registerPassword.value,
+      this.registerbirthdate.value);
 
     // despues borrar todos los console.log
 
