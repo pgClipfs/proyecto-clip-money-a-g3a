4701257ebab058f21467b4e6379ff2333dfe6e60
M. Ferrero <69654913+FerreroM95@users.noreply.github.com>
2021-01-03 18:00:22 -0300

    mi ultimo commit - fileuploadingcontroller

---
+++ Clip-Back/Clip-Back/Controllers/FileUploadingController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Clip_Back.Controllers
+{
+    public class FileUploadingController : ApiController
+    {
+
+        [HttpPost]
+        [Route("api/FileUploading/UploadFile")]
+        public async Task<HttpResponseMessage> UploadFile() 
+        {
+            var ctx = HttpContext.Current;
+            var root = ctx.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider
+                (root);
+            try 
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+                foreach(var file in provider.FileData) 
+                {
+                    var name = file.Headers.ContentDisposition.FileName;
+
+                    name = name.Trim('"');
+
+                    var localFileName = file.LocalFileName;
+                    var filePath = Path.Combine(root, name);
+
+                    File.Move(localFileName, filePath);
+                }
+
+            }
+            catch (Exception e) 
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
